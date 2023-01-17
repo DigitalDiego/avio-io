31ad7704b4e846f33001f0547982985477f38f86
Diego Hernandez <diegohxxiv@gmail.com>
2023-01-17 13:49:21 -0800

    Comment styles updated

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,7 +5,9 @@ import moment from "moment";
 export default function Comment(props) {
   return (
     <div className="w-full flex flex-col gap-2">
-      <p>{props?.comment}</p>
+      <div className="border-t-solid border-t-[1px] border-t-gray-500 w-full min-h-[10vh] flex justify-start items-start py-2">
+        <p>{props?.comment}</p>
+      </div>
       <div className="w-full flex justify-between items-center">
         <Link
           className="flex items-center gap-2"
